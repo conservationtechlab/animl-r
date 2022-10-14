f4cfeb3a930203b141bf8c656a79345135c13dcf
Mathias Tobler <matobler@gmx.net>
2022-10-13 20:12:58 -0700

    Renamed resize_pad to resizePad and moved to extractBoxes.R since it is only being used there.

--- R/extractBoxes.R
+++ R/extractBoxes.R
@@ -72,7 +72,7 @@ extractBoxesFromMD <- function(image, min_conf = 0, buffer = 0, return.crops = F
 
       # resize and pad if requested
       if (!is.na(resize)) {
-        crop <- resize_pad(crop, resize)
+        crop <- resizePad(crop, resize)
       }
 
       # if we return crops save crop in list
@@ -179,7 +179,7 @@ extractBoxesFromFlat <- function(image, min_conf = 0, buffer = 0, save = TRUE, r
   
     # resize and pad if requested
     if (!is.na(resize)) {
-      crop <- resize_pad(crop, resize)
+      crop <- resizePad(crop, resize)
     }
   
     # save image if requested
@@ -252,7 +252,7 @@ extractBoxes <- function(images, min_conf = 0, buffer = 0, save = FALSE, resize 
       type <- "PSOCK"
       
       cl <- parallel::makeCluster(min(parallel::detectCores(), nproc), type = type)
-      parallel::clusterExport(cl, list("buffer", "resize", "quality", "outdir", "images", "extractBoxesFromMD", "resize_pad"), envir = environment())
+      parallel::clusterExport(cl, list("buffer", "resize", "quality", "outdir", "images", "extractBoxesFromMD", "resizePad"), envir = environment())
       # set random number generator for cluster
       parallel::clusterSetRNGStream(cl)
       
@@ -281,7 +281,7 @@ extractBoxes <- function(images, min_conf = 0, buffer = 0, save = FALSE, resize 
     if (parallel) {
       type <- "PSOCK"
       cl <- parallel::makeCluster(min(parallel::detectCores(), nproc), type = type)
-      parallel::clusterExport(cl, list("buffer", "resize", "quality", "outdir", "images", "extractBoxesFromFlat", "resize_pad"), envir = environment())
+      parallel::clusterExport(cl, list("buffer", "resize", "quality", "outdir", "images", "extractBoxesFromFlat", "resizePad"), envir = environment())
       # set random number generator for cluster
       parallel::clusterSetRNGStream(cl)
       
@@ -294,3 +294,28 @@ extractBoxes <- function(images, min_conf = 0, buffer = 0, save = FALSE, resize 
     results    
   }
 }
+
+#' Resize an image with padding
+#'
+#' @param img  the image, read by jpeg library
+#' @param size new size
+#'
+#' @return returns resized jpeg image
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' crop <- resizePad(cropped_image_path,256)
+#' }
+resizePad <- function(img, size = 256) {
+  if (dim(img)[0] == 0 || dim(img)[1] == 0) {
+    return(img)
+  }
+  imgpad <- array(0, c(max(dim(img)), max(dim(img)), 3))
+  xstart <- max(1, floor((dim(imgpad)[2] - dim(img)[2]) / 2))
+  ystart <- max(1, floor((dim(imgpad)[1] - dim(img)[1]) / 2))
+  imgpad[ystart:(ystart + dim(img)[1] - 1), xstart:(xstart + dim(img)[2] - 1), ] <- img
+  imgres <- imager::resize(imager::as.cimg(imgpad), size_x = size, size_y = size, interpolation_type = 3)
+  imgres[, , 1, ]
+}
+
