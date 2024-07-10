13a8c5e96cad6e8ae16f24fcb8118b81f37d40cf
Kyra Swanson <t.kyraswanson@gmail.com>
2024-07-10 16:04:54 -0700

    update description, namespace

--- R/classification.R
+++ R/classification.R
@@ -1,13 +1,14 @@
-#' Title
+#' Load a Classifier Model with animl-py
 #'
 #' @param model_path path to model
 #' @param class_file path to class list
 #' @param device send model to the specified device
 #'
-#' @return
+#' @return list of [classifier, class_list]
 #' @export
 #'
 #' @examples
+#' \dontrun{andes <- loadModel('andes_v1.pt','andes_classes.csv')}
 loadModel <- function(model_path, class_file, device="gpu"){
   if(py_module_available("animl")){
     animl_py <- import("animl")
@@ -19,26 +20,27 @@ loadModel <- function(model_path, class_file, device="gpu"){
 }
 
 
-#' Title
+#' Infer Species for Given Detections
 #'
-#' @param detections 
-#' @param model 
-#' @param classes 
-#' @param device 
-#' @param out_file 
-#' @param file_col 
-#' @param crop 
-#' @param resize 
-#' @param standardize 
-#' @param batch_size 
-#' @param workers 
-#' @param channel_last 
-#' @param raw 
+#' @param detections manifest of animal detections
+#' @param model loaded classifier model
+#' @param classes data.frame of classes
+#' @param device send model to the specified device
+#' @param out_file path to csv to save results to
+#' @param file_col column in manifest containing file paths
+#' @param crop use bbox to crop images before feeding into model
+#' @param resize model expected dimensions
+#' @param standardize standardize color
+#' @param batch_size batch size for generator 
+#' @param workers number of processes 
+#' @param channel_last change matrix to BxWxHxC
+#' @param raw output raw logits in addition to manifest
 #'
-#' @return
+#' @return detections manifest with added prediction and confidence columns
 #' @export
 #'
 #' @examples
+#' \dontrun{animals <- predictSpecies(animals, classifier[[1]], classifier[[2]], raw=FALSE)}
 predictSpecies <- function(detections, model, classes, device='gpu', out_file=NULL,
                            file_col='Frame', crop=TRUE, resize=299, standardize=TRUE,
                            batch_size=1, workers=1, channel_last=FALSE, raw=FALSE){
