baa418302ecfa6d2448f9d36838298b91fc7d94b
tkswanson <t.kyraswanson@gmail.com>
2025-09-07 21:01:43 -0700

    add download_model to utils

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,31 @@ load_animl_py <- function() {
   else{ stop('animl-py environment must be loaded first via reticulate') }
   return(animl_py)
 }
+
+
+#' Download a given model
+#'
+#' @param model_url url of the model to download
+#' @param out_dir Directory to save the model.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+download_model <- function(model_url, out_dir='models'){
+  animl_py <- load_animl_py()
+  animl_py$download_model(model_url, outdir=out_dir)
+  
+}
+
+
+#' List models available for download
+#'
+#' @return
+#' @export
+#'
+#' @examples
+list_models <- function(){
+  animl_py <- load_animl_py()
+  animl_py$list_models
+}
