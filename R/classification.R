ab8b2eb6ab78cc81453cbd194166a2296f45e1a1
tkswanson <t.kyraswanson@gmail.com>
2026-01-29 15:07:10 -0800

    add best param to single_classification

--- R/classification.R
+++ R/classification.R
@@ -1,3 +1,22 @@
+#' Save model state weights
+#'
+#' @param model pytorch model
+#' @param out_dir directory to save model to
+#' @param epoch  current training epoch
+#' @param stats performance metrics of current epoch
+#' @param optimizer pytorch optimizer (optional)
+#' @param scheduler pytorch scheduler (optional)
+#'
+#' @returns None
+#' @export
+#'
+#' @examples
+#' \dontrun{save_classifier(model, 'models/', 10, list(acc = 0.85))}
+save_classifier <- function(model, out_dir, epoch, stats, optimizer=NULL, scheduler=NULL){
+  animl_py <- get("animl_py", envir = parent.env(environment()))
+  animl_py$save_classifier(model, out_dir, epoch, reticulate::r_to_py(stats), optimizer=optimizer, scheduler=scheduler)
+}
+
 #' Load a Classifier Model and Class_list
 #'
 #' @param model_path path to model
@@ -19,25 +38,6 @@ load_classifier <- function(model_path, classes, device=NULL, architecture="CTL"
   animl_py$load_classifier(model_path, classes, device=device, architecture=architecture)
 }
 
-
-#' Save model state weights
-#'
-#' @param model pytorch model
-#' @param out_dir directory to save model to
-#' @param epoch  current training epoch
-#' @param stats performance metrics of current epoch
-#' @param optimizer pytorch optimizer (optional)
-#' @param scheduler pytorch scheduler (optional)
-#'
-#' @returns None
-#' @export
-#'
-#' @examples
-#' \dontrun{save_classifier(model, 'models/', 10, list(acc = 0.85))}
-save_classifier <- function(model, out_dir, epoch, stats, optimizer=NULL, scheduler=NULL){
-  animl_py <- get("animl_py", envir = parent.env(environment()))
-  animl_py$save_classifier(model, out_dir, epoch, reticulate::r_to_py(stats), optimizer=optimizer, scheduler=scheduler)
-}
 
 #' Load class list .csv file
 #'
@@ -73,11 +73,10 @@ load_class_list <- function(classlist_file){
 #' @examples
 #' \dontrun{animals <- classify(classifier, animals, file_col='filepath')}
 classify <- function(model, detections, 
-                       resize_width=480, resize_height=480,
-                       file_col='filepath', crop=TRUE, normalize=TRUE,
-                       batch_size=1, num_workers=1,
-                       device=NULL, out_file=NULL){
-  
+                     resize_width=480, resize_height=480,
+                     file_col='filepath', crop=TRUE, normalize=TRUE,
+                     batch_size=1, num_workers=1,
+                     device=NULL, out_file=NULL){
     animl_py <- get("animl_py", envir = parent.env(environment()))
     animl_py$classify(model, detections,
                       resize_width=as.integer(resize_width),
@@ -95,13 +94,14 @@ classify <- function(model, detections,
 #' @param empty manifest of md human, vehicle and empty images
 #' @param predictions_raw softmaxed likelihoods from predict_species
 #' @param class_list list of class labels
+#' @param best whether to return one prediction per file
 #'
 #' @returns dataframe with prediction and confidence columns
 #' @export
 #'
 #' @examples
 #' \dontrun{animals <- single_classification(animals, empty, pred_raw, class_list)}
-single_classification <- function(animals, empty, predictions_raw, class_list){
+single_classification <- function(animals, empty, predictions_raw, class_list, best=FALSE){
   animl_py <- get("animl_py", envir = parent.env(environment()))
-  animl_py$single_classification(animals, empty, predictions_raw, class_list)
+  animl_py$single_classification(animals, empty, predictions_raw, class_list, best=best)
 }
