1398550c1e093453052aff507f0eff22ecc2d4eb
tkswanson <t.kyraswanson@gmail.com>
2026-05-14 10:15:20 -0700

    adding get_empty and get_animals back for now

--- R/detection.R
+++ R/detection.R
@@ -102,3 +102,63 @@ parse_detections <- function(results,
                             threshold=threshold,
                             file_col=file_col)
 }
+
+
+#' Return MD empty, vehicle and human images in a dataframe
+#'
+#' @param manifest all megadetector frames
+#'
+#' @return list of empty/human/vehicle allframes with md classification
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' empty <- get_empty(imagesall)
+#' }
+get_empty <- function(manifest) {
+  if (!is(manifest, "data.frame")) { stop("'manifest' must be Data Frame")}
+  
+  # fill md empties with 0
+  manifest$category[is.na(manifest$category)] <- 0
+  empty <- manifest[manifest$category != 1, ]
+  
+  if (nrow(empty) == 0) {
+    empty <- data.frame(matrix(ncol = ncol(manifest), nrow = 0))
+    colnames(empty) <- names(manifest)
+    return(empty)
+  }
+  empty$prediction <- NA
+  empty$confidence <- NA
+  
+  categories <- unique(manifest$category)
+  if (0 %in% categories) {
+    empty[which(empty$category == 0), "prediction"] <- "empty"
+    empty[which(empty$category == 0), "confidence"] <- 1
+  }
+  if (2 %in% categories) {
+    empty[which(empty$category == 2), "prediction"] <- "human"
+    empty[which(empty$category == 2), "confidence"] <- empty$conf[which(empty$category == 2)]
+  }
+  if (3 %in% categories) {
+    empty[which(empty$category == 3), "prediction"] <- "vehicle"
+    empty[which(empty$category == 3), "confidence"] <- empty$conf[which(empty$category == 3)]
+  }
+  return(empty)
+}
+
+
+#' Return a dataframe of only MD animals
+#'
+#' @param manifest all megadetector frames
+#'
+#' @return animal frames classified by MD
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' animals <- get_animals(imagesall)
+#' }
+get_animals <- function(manifest){
+  if (!is(manifest, "data.frame")) { stop("'manifest' must be Data Frame")}
+  return(manifest[which(manifest$category == 1), , drop = FALSE])
+}
